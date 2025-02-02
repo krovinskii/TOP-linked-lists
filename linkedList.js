9fde1f6e2d64695d1a750b884b1fa9e4e1d9c3b3
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-02-02 12:00:53 -0600

    Done

--- linkedList.js
+++ linkedList.js
@@ -11,8 +11,8 @@ class LinkedList {
       this.head = newNode;
       this.tail = newNode;
     } else {
-      this.tail = newNode;
       this.tail.nextNode = newNode;
+      this.tail = newNode;
     }
     this.length++;
   }
@@ -57,10 +57,63 @@ class LinkedList {
 
     return currentNode.value;
   }
-  pop() {} // removes the last element from the list
-  contains(value) {} //returns true if the passed in value is in the list and otherwise returns false.
-  find(value) {} //returns the index of the node containing value, or null if not found.
-  toString() {} //represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
+  pop() {
+    if (this.length === 0) {
+      return;
+    }
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let currentNode = this.head;
+
+      while (currentNode.nextNode !== this.tail) {
+        currentNode = currentNode.nextNode;
+      }
+
+      currentNode.nextNode = null;
+
+      this.tail = currentNode;
+    }
+
+    this.length--;
+  }
+
+  contains(value) {
+    //returns true if the passed in value is in the list and otherwise returns false.
+    let currentNode = this.head;
+    for (let i = 0; i < this.length; i++) {
+      if (currentNode.value === value) {
+        return true;
+      } else {
+        currentNode = currentNode.nextNode;
+      }
+    }
+    return false;
+  }
+  find(value) {
+    //returns the index of the node containing value, or null if not found.
+    let currentNode = this.head;
+    for (let i = 0; i < this.length; i++) {
+      if (currentNode.value === value) {
+        return i;
+      } else {
+        currentNode = currentNode.nextNode;
+      }
+    }
+    return null;
+  }
+  toString() {
+    //represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
+    let currentNode = this.head;
+    let result = "";
+    while (currentNode) {
+      result += `( ${currentNode.value} ) -> `;
+      currentNode = currentNode.nextNode;
+    }
+    return result + "null";
+  }
 }
 
 class Node {
@@ -69,3 +122,15 @@ class Node {
     this.nextNode = nextNode;
   }
 }
+
+// example uses class syntax - adjust as necessary
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+console.log(list);
+console.log(list.toString());
